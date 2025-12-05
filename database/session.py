c64c6a8c026f7d1d427741a838b89ea8487ab2b9
kaveh nasiri <kavehnasirilari@gmail.com>
2025-12-05 23:11:32 +0330

    Add SQLAlchemy Base, engin and session factory (Phase 2)

---
+++ database/session.py
@@ -0,0 +1,29 @@
+# database/session.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+#-------------------------------------------------------------
+# NOTE for reviewers / futur maintainers:
+# in phase 2 we hardcode DATABASE_URL only for local development
+# and ambelic migration initialization.
+
+# TODO (phase 5-6)
+#   -move DATABASE_URL to environment variables (.env)
+#   -configure docker-compose to inject env values
+#   -replace this hardcode string with `os.getenv("DATABASE_URL")`
+#   -insure engin works in both local & docker environments
+#-----------------------------------------------------------------
+
+
+class Base(DeclarativeBase):
+    pass
+
+#engin
+DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost:5432/market_data"
+
+engin= create_engine(DATABASE_URL, echo=True)
+
+#Session Facotry
+SessionLocal = sessionmaker(bind=engin, autoflush=False, autocommit=False)
+
+
