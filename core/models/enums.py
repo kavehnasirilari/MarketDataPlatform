c69341cb7fa19ae2b834f7f30004a92089ce12b8
kaveh nasiri <kavehnasirilari@gmail.com>
2025-12-07 21:25:42 +0330

    feat(core): add Exchange, MarketType, Interval enums for canonical model

---
+++ core/models/enums.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+class Exchange(Enum):
+    BINANCE = "binance"
+    COINBASE = "coinbase"
+    HYPERLIQUID = "hyperliquid"
+
+
+class MarketType(Enum):
+    SPOT = "spot"
+    FUTURES = "futures"
+    PREP = "prep"
+
+class interval(Enum):
+    M1 = "1m"
+    M5 = "5m"
+    M15 = "15m"
+    H1 = "1h"
+    H4 = "4h"
+    D1 = "1d"
+    W1 = "1w"
