1139dbed57cd93876c60da2b8c4d6550fed30dd4
kaveh nasiri <kavehnasirilari@gmail.com>
2026-04-16 21:52:23 +0330

    feat(api_service): introduce semantics layer for request/response validation and transformation

--- api_service/app/semantics/mock_impl.py
+++ api_service/app/semantics/mock_impl.py
@@ -1,14 +1,27 @@
 #api_service/app/semantics/mock_impl.py
 
 from api_service.app.semantics.base import SemanticType, SemanticResult, SemanticAnnotator
-
+from api_service.app.policy.base import PolicyDecisionType
 
 class MockSemanticAnnotator(SemanticAnnotator):
     def annotate(self, policy_decision=None, data_result=None) -> SemanticResult:
+
+        if policy_decision.decision == PolicyDecisionType.DENY:
+            return SemanticResult(
+                type = SemanticType.ERROR.value,
+                message= policy_decision.reason
+            )
+
+        if data_result.available == False:      
+            return SemanticResult(
+                 type = SemanticType.ERROR.value,
+                message= data_result.message
+            )        
+        
         return SemanticResult(
-            type = SemanticType.SUCCESS,
+            type = SemanticType.SUCCESS.value,
             message= None,
-            data= None
+            data= data_result.payload
         ) 
 
 
